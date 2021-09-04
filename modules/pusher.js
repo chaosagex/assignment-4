702b8dcaa9691f72bcbe2c5a66b406a27142c961
DESKTOP-AASTOLS\Hossa <hk2nasr@gmail.com>
2021-09-05 00:59:24 +0200

    forgot to push app.... added functional? pusher chatting app

---
+++ modules/pusher.js
@@ -0,0 +1,86 @@
+Pusher.logToConsole = true;
+export var msgs=[];
+let addMsg=function(msg) {
+   msgs.push(msg)
+}
+export class PusherHandler {
+
+    constructor(userName, group) {
+        this.event = "msg";
+        this.secret = '7d70f7639ec7b58c30a0';
+        this.key = "448486c547d013582aeb";
+        this.app = "1261092";
+        this.numberUsers = 0;
+        this.userName = userName + "@" + Math.random().toString(16).slice(2);
+        this.group = group;
+        this.pusher = new Pusher(this.key, {
+            cluster: 'eu',
+        });
+        this.presence = null;
+        this.channel = null;
+        this.subscribeTo(group);
+        // if (!document.cookie.match("(^|;) ?user_id=([^;]*)(;|$)")) {
+        //     // Primitive auth! This 'user_id' cookie is read by your auth endpoint,
+        //     // and used as the user_id in the subscription to the 'presence-quickstart'
+        //     // channel. This is then displayed to all users in the user list.
+        //     // In your production app, you should use a secure auth system.
+        //     document.cookie = "user_id=" + this.userName;
+        // }
+
+    }
+    addMember() {
+        this.numberUsers++;
+    }
+    removeMember() {
+        this.numberUsers--;
+    }
+    
+    subscribeTo(group) {
+        this.channel = this.pusher.subscribe(group.toLowerCase());
+        this.channel.bind(this.event, function (data) {
+            addMsg(JSON.stringify(data));
+        })
+        // this.presence = this.pusher.subscribe(`presence-${group.toLowerCase()}`)
+        // this.presence.bind("pusher:subscription_succeeded", () =>
+        //     this.presence.members.each(this.addMember()));
+        // this.presence.bind("pusher:member_added", this.addMember());
+        // this.presence.bind("pusher:member_removed", (this.removeMember()));
+    }
+    unSubscribe(group) {
+        this.channel = this.pusher.unsubscribe(group.toLowerCase());
+        this.presence = this.pusher.unsubscribe(`presence-${group.toLowerCase()}`)
+        this.channel = null;
+        this.presence = null;
+    }
+    disconnect(group) {
+        this.presence = this.pusher.unsubscribe(`presence-${group.toLowerCase()}`)
+        this.channel = null;
+        this.presence = null;
+    }
+    countUsers() {
+        return this.numberUsers
+    }
+    getMD5(body) {
+        return CryptoJS.MD5(JSON.stringify(body));
+    }
+    getAuthSignature(md5, timeStamp) {
+        return CryptoJS.HmacSHA256(`POST\n/apps/${this.app}/events\nauth_key=${this.key}&auth_timestamp=${timeStamp}&auth_version=1.0&body_md5=${md5}`, this.secret);
+    }
+    sendMessage = async function (message) {
+        let body = { data: `${this.userName}!${message}`, name: this.event, channel: this.group }
+        let timeStamp = Date.now() / 1000;
+        let md5 = this.getMD5(body);
+        //https://cors.bridged.cc/
+        let url = `https://cors.bridged.cc/https://api-eu.pusher.com/apps/${this.app}/events?body_md5=${md5}&auth_version=1.0&auth_key=${this.key}&auth_timestamp=${timeStamp}&auth_signature=${this.getAuthSignature(md5, timeStamp)}`;
+        let req = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+    }
+
+}
+
---
+++ script.js
@@ -0,0 +1,93 @@
+import * as push from "./modules/pusher.js";
+var pusher = null;
+var timer=null;
+var users = 0;
+var group = "";
+const screen1 = document.querySelector("#Screen_1")
+const screen2 = document.querySelector("#Screen_2")
+screen2.setAttribute("style", "display:none;");
+const currentGroup = document.querySelector("#group");
+currentGroup.textContent = `${group}:online users: ${users}`;
+var currentTime = 0;
+const time = document.querySelector("#timer");
+const joinbutton=document.querySelector("#add");
+const chatList=document.querySelector("#chatlist");
+const logoutButton=document.querySelector("#logout");
+const sendButton=document.querySelector("#send");
+
+
+const join = function () {
+    let name = document.querySelector("#username").value;
+    group = document.querySelector("#channel").value;
+    if (name && group) {
+        pusher = new push.PusherHandler(name, group);
+        users = pusher.numberUsers;
+        currentTime = 60;
+        timer=setInterval(function(){ updateTimer(); }, 1000)
+        let screen1 = document.querySelector("#Screen_1")
+        screen1.setAttribute("style", "display:none;");
+        screen2.setAttribute("style", "");
+    }
+}
+
+
+
+const updatechat=function() {
+    let origin=pusher.userName;
+    let usersSet=new Set();
+    usersSet.add(origin);
+    let chatList = document.querySelector("#chatlist");
+    chatList.innerHTML=`<ul id="chatlist" class="list-group overflow-auto w-75 h-50 text-center"></ul>`;
+    let user = origin.split("@")[0];
+    push.msgs.forEach(element => {
+    let elemOrigin=element.split("!")[0];
+    let elemuser=elemOrigin.split("@")[0];
+    let msg=element.split("!")[1];
+    let tem=elemOrigin.substring(1);
+    if (tem === origin) chatList.innerHTML += `<li class="list-group-item text-xxl-end">${elemuser}:
+        ${msg}</li>`;
+
+    else {
+        chatList.innerHTML += `<li class="list-group-item text-xxl-start">${elemuser}:
+    ${msg}</li>`;
+    usersSet.add(elemOrigin);
+    }
+    users=usersSet.size;
+    currentGroup.textContent = `${group}:online users: ${users}`;
+    });
+    
+
+}
+const sendMessage=function(){
+    let message=document.querySelector("#msg").value;
+    pusher.sendMessage(message);
+    currentTime=60;
+}
+
+let logout = function () {
+    screen2.setAttribute("style", "display:none;");
+    screen1.setAttribute("style", "");
+    clearInterval(timer);
+    timer=null;
+    currentTime = 0;
+    chatList.innerHTML=`<ul id="chatlist" class="list-group overflow-auto w-75 h-50 text-center"></ul>`
+}
+let setTimerLabel = function () {
+
+    time.textContent = currentTime < 60 ? `You will logout after: (0:${currentTime})` : `You will logout after: (1:00)`;
+}
+let updateTimer = function () {
+    currentTime--;
+    updatechat()
+    if (currentTime < 0) {
+        logout();
+        clearInterval(timer);
+    }
+    else {
+        setTimerLabel();
+
+    }
+}
+joinbutton.addEventListener("click",function(){ join(); });
+logoutButton.addEventListener("click",function(){ logout(); });
+sendButton.addEventListener("click",function(){ sendMessage(); });
